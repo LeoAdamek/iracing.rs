ff25e0fd96057eef33f49a7a314f0e1a0a526967
LeoAdamek <leo@adamek.me>
2020-03-01 19:45:35 +0000

    M'map

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,8 @@ extern crate bitflags;
 
 extern crate memmap;
 
-use memmap::*;
+use memmap::Mmap;
+use std::fs::OpenOptions;
 
 pub mod session;
 pub mod telemetry;
@@ -12,16 +13,36 @@ pub const TELEMETRY_PATH: &str = "Local\\IRSDKMemMapFileName";
 pub const UNLIMITED_LAPS: i32 = 32767;
 pub const UNLIMITED_TIME: f32 = 604800.0;
 
-pub fn open() -> Result<Client, String> {
+pub fn connect() -> std::io::Result<Client> {
+    let file = OpenOptions::new()
+        .read(true)
+        .write(false)
+        .create(false)
+        .open(TELEMETRY_PATH)?;
 
+    let _ = unsafe {
+        Mmap::map(&file)?;
+    };
+
+    return Ok(Client::new());
 }
 
-pub struct Client { }
+pub struct Client { 
+}
+
+impl Client {
+    pub fn new() -> Client {
+        Client { }
+    }
+}
 
 #[cfg(test)]
 mod tests {
+    use super::connect;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_connect() {
+        connect().expect("Unable to do the telemetry");
+        assert_eq!(1,1);
     }
 }
