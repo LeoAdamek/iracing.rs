4f69295c49f1d1fb525b2e0c28b411836283cdbb
LeoAdamek <leo@adamek.me>
2020-03-01 17:39:59 +0000

    M'Map

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,22 @@
 #[macro_use]
 extern crate bitflags;
 
-pub mod telemetry;
+extern crate memmap;
+
+use memmap::*;
+
 pub mod session;
+pub mod telemetry;
 
-pub const 
+pub const TELEMETRY_PATH: &str = "Local\\IRSDKMemMapFileName";
+pub const UNLIMITED_LAPS: i32 = 32767;
+pub const UNLIMITED_TIME: f32 = 604800.0;
 
+pub fn open() -> Result<Client, String> {
+
+}
+
+pub struct Client { }
 
 #[cfg(test)]
 mod tests {
