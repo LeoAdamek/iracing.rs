1dc878469559abf55b9e7b42b870d23c37d83f62
Leo Adamek <leo@adamek.me>
2020-03-02 19:22:14 +0000

    We can now read the header

--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,109 @@
 #[macro_use]
 extern crate bitflags;
 
-extern crate memmap;
+extern crate winapi;
 
-use memmap::Mmap;
-use std::fs::OpenOptions;
+use crate::session::Header;
+use std::ptr::null;
+use std::io::Error;
+use std::io::Result as IOResult;
+use std::os::windows::raw::HANDLE;
+use winapi::shared::minwindef::LPVOID;
+use winapi::um::errhandlingapi::GetLastError;
+use winapi::um::memoryapi::{OpenFileMappingW, FILE_MAP_READ, MapViewOfFile};
 
 pub mod session;
 pub mod telemetry;
 
-pub const TELEMETRY_PATH: &str = "Local\\IRSDKMemMapFileName";
+pub const TELEMETRY_PATH: &'static str = "Local\\IRSDKMemMapFileName";
 pub const UNLIMITED_LAPS: i32 = 32767;
 pub const UNLIMITED_TIME: f32 = 604800.0;
 
-pub fn connect() -> std::io::Result<Client> {
-    let file = OpenOptions::new()
-        .read(true)
-        .write(false)
-        .create(false)
-        .open(TELEMETRY_PATH)?;
 
-    let _ = unsafe {
-        Mmap::map(&file)?;
-    };
-
-    return Ok(Client::new());
-}
-
-pub struct Client { 
+pub struct Client {
+    conn: Connection,
 }
 
 impl Client {
-    pub fn new() -> Client {
-        Client { }
+    pub fn new(conn: Connection) -> Client {
+        return Client { conn: conn };
     }
+
+    pub fn header(&mut self) -> Header {
+        self.conn.header()
+    }
+}
+
+pub struct Connection {
+    location: *mut std::ffi::c_void
+}
+
+impl Connection {
+    pub fn new() -> IOResult<Connection> {
+        let mut path: Vec<u16> = TELEMETRY_PATH.encode_utf16().collect();
+        path.push(0);
+
+        let mapping: HANDLE;
+        let errno: i32;
+
+        unsafe { mapping = OpenFileMappingW(FILE_MAP_READ, 0, path.as_ptr()); };
+
+        if null() == mapping {
+            
+            unsafe { errno = GetLastError() as i32; }
+
+            return Err(Error::from_raw_os_error(errno));
+        }
+
+        let view: LPVOID;
+ 
+        unsafe {
+            view = MapViewOfFile(mapping, FILE_MAP_READ, 0, 0, 0);
+        }
+
+        if null() == view {
+            unsafe { errno = GetLastError() as i32; }
+
+            return Err(Error::from_raw_os_error(errno))
+        }
+
+        return Ok(Connection {location: view});
+    }
+
+    /**
+     * Read the contents of the memory map
+     */
+    pub fn header (&mut self) -> Header {
+        let raw_header: *const Header = unsafe { std::mem::transmute(self.location) };
+        let h: Header = unsafe { *raw_header };
+
+        h.clone()
+    }
+}
+
+pub fn connect() -> IOResult<Client> {
+    let conn = Connection::new()?;
+
+    return Ok(Client::new(conn));
 }
 
 #[cfg(test)]
 mod tests {
-    use super::connect;
+    use super::*;
 
     #[test]
     fn test_connect() {
-        connect().expect("Unable to do the telemetry");
-        assert_eq!(1,1);
+        println!("Opening telemetry at {}", TELEMETRY_PATH);
+        connect().expect("Unable to open telemetry");
+        assert_eq!(1, 1);
+    }
+
+    #[test]
+    fn test_header() {
+        println!("Sampling Telemetry");
+
+        let header: Header = connect().expect("Unable to open telemetry").header();
+
+        assert_eq!(header.version, 2);
     }
 }
--- src/session.rs
+++ src/session.rs
@@ -1,43 +1,71 @@
-mod session {
-    pub struct SessionDetails {
-        pub track_name: &'static str,               // Track Name
-        pub track_id: u32,                          // iRacing Track ID
-        pub track_length: f32,                      // Track length (km)
-        pub track_display_name: &'static str,       // Track display name
-        pub track_display_short_name: &'static str, // Track short display name
-        pub track_config_name: &'static str,        // Track layout/configuration name
-        pub track_city: &'static str,               // Track Location, City
-        pub track_country: &'static str,            // Track Location: Country
-        pub track_altitude: f32,                    // Track Altitude (m)
-        pub track_latitude: f64,                    // Track Latitude (deg)
-        pub track_longitude: f64,                   // Track Longitude (deg)
-        pub track_north_offset: f32,                // Track rotation relative to true north (rad)
-        pub track_turns: u32,                       // Number of turns
-        pub track_pit_speed_limit: f32,             // Pit speed limit (km/h)
-        pub track_type: &'static str,               // Track type (Road, Oval, Dirt, DOval)
-        pub track_weather: &'static str,            // Track Weather
-        pub track_skies: &'static str,              // Sky state
-        pub track_surface_temperature: f32,         // Track surface temperature (degC)
-        pub track_air_tempearture: f32,             // Track air temperature (degC)
-        pub track_air_pressure: f32,                // Track air pressure (Hg)
-        pub track_wind_speed: f32,                  // Track wind speed (km/h)
-        pub track_wind_direction: f32,              // Track wind direction relative to north (rad)
-        pub track_fog: f32,                         // Track fogginess
-        pub track_cleanup: i32,                     // Track cleanup
-        pub track_dynamic: i32,                     // Track Dynamic
-        pub series_id: i32,                         // iRacing series ID
-        pub season_id: i32,                         // iRacing season ID
-        pub session_id: i32,                        // iRacing session Id
-        pub sub_session_id: i32,                    // iRacing subsession (split) ID
-        pub league_id: i32,                         // iRacing League ID
-        pub official: i8,                           // Official Race
-        pub race_week: i32,                         // Race  Week Number
-        pub event_type: &'static str,               // Event Type
-        pub category: &'static str,                  // Category
-        pub sim_mode: &'static str,                          // Sim Mode
-        pub team_race: i8,                          // Is Team Race
-        pub min_drivers: i8,                        // Minimum drivers per team
-        pub max_drivers: i8,                        // Maximum drivers per team
-        pub dc_rule_set: str,                       // Driver change rules
-    }
+
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct Header {
+    pub version: i32, // Telemetry version
+    pub status: i32, // Status
+    pub tick_rate: i32, // Tick rate (Hz)
+    
+    pub session_info_version: i32, // Increments each time session info is updated
+    pub session_info_length: i32, // Length of session info data
+    pub session_info_offset: i32, // Offset of session info data
+    
+    pub n_vars: i32, // Number of values
+    pub header_offset: i32, // Offset to header variables
+
+    pub n_buffers: i32, // # of buffers (<= 3 for now)
+    pub buffer_length: i32, // Length per line
+    pub padding: [u32; 2], // Padding
+
+    pub buffers: [ValueBuffer; 4]  // Data buffers
+}
+
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct ValueBuffer {
+    pub ticks: i32, // Tick count
+    pub offset: i32, // Offset
+    pub padding: [u32; 2] // (16-byte align) Padding
+}
+
+pub struct SessionDetails {
+    pub track_name: &'static str,               // Track Name
+    pub track_id: u32,                          // iRacing Track ID
+    pub track_length: f32,                      // Track length (km)
+    pub track_display_name: &'static str,       // Track display name
+    pub track_display_short_name: &'static str, // Track short display name
+    pub track_config_name: &'static str,        // Track layout/configuration name
+    pub track_city: &'static str,               // Track Location, City
+    pub track_country: &'static str,            // Track Location: Country
+    pub track_altitude: f32,                    // Track Altitude (m)
+    pub track_latitude: f64,                    // Track Latitude (deg)
+    pub track_longitude: f64,                   // Track Longitude (deg)
+    pub track_north_offset: f32,                // Track rotation relative to true north (rad)
+    pub track_turns: u32,                       // Number of turns
+    pub track_pit_speed_limit: f32,             // Pit speed limit (km/h)
+    pub track_type: &'static str,               // Track type (Road, Oval, Dirt, DOval)
+    pub track_weather: &'static str,            // Track Weather
+    pub track_skies: &'static str,              // Sky state
+    pub track_surface_temperature: f32,         // Track surface temperature (degC)
+    pub track_air_tempearture: f32,             // Track air temperature (degC)
+    pub track_air_pressure: f32,                // Track air pressure (Hg)
+    pub track_wind_speed: f32,                  // Track wind speed (km/h)
+    pub track_wind_direction: f32,              // Track wind direction relative to north (rad)
+    pub track_fog: f32,                         // Track fogginess
+    pub track_cleanup: i32,                     // Track cleanup
+    pub track_dynamic: i32,                     // Track Dynamic
+    pub series_id: i32,                         // iRacing series ID
+    pub season_id: i32,                         // iRacing season ID
+    pub session_id: i32,                        // iRacing session Id
+    pub sub_session_id: i32,                    // iRacing subsession (split) ID
+    pub league_id: i32,                         // iRacing League ID
+    pub official: i8,                           // Official Race
+    pub race_week: i32,                         // Race  Week Number
+    pub event_type: &'static str,               // Event Type
+    pub category: &'static str,                 // Category
+    pub sim_mode: &'static str,                 // Sim Mode
+    pub team_race: i8,                          // Is Team Race
+    pub min_drivers: i8,                        // Minimum drivers per team
+    pub max_drivers: i8,                        // Maximum drivers per team
+    pub dc_rule_set: str,                       // Driver change rules
 }
