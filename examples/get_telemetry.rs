481f513ad03da5d2dd82ba4090041f6cbd8b6af3
Leo Adamek <leo@adamek.me>
2020-03-09 20:33:45 +0000

    Tweak get_telemetry example

--- examples/get_telemetry.rs
+++ examples/get_telemetry.rs
@@ -1,13 +1,14 @@
 use iracing::Connection;
+use iracing::states::Flags;
 use std::time::Duration;
 
 pub fn main() {    
     let mut conn = Connection::new().expect("Unable to open telemetry");
 
     let session = conn.session_info().expect("Invalid Session");
-    let shift_up_rpm = session.drivers.shift_light_shift_rpm;
 
-    let blocking = unsafe { conn.blocking() }.expect("Couldn't create telem handle");
+    let shift_up_rpm = session.drivers.shift_light_shift_rpm;
+    let blocking = conn.blocking().expect("Couldn't create telem handle");
 
     loop {
         let telem = match blocking.sample(Duration::from_millis(500)) {
@@ -23,11 +24,16 @@ pub fn main() {
         let timecode: f64 = telem.get("SessionTime").unwrap().into();
         let lap: i32 = telem.get("Lap").unwrap().into();
 
-        print!("Lap {lap:>3}: {time:>5.3}s Gear {gear} @ {rpm:>5.0} RPM", lap = lap, gear = gear, rpm = rpm, time = timecode);
+        let flags = Flags::from_bits( telem.get("SessionState").unwrap().into() ).unwrap();
+
+
+        print!("Lap {lap:>3}: {time:>5.3}s Gear {gear} @ {rpm:>5.0} RPM ", lap = lap, gear = gear, rpm = rpm, time = timecode);
         
         if shift_up_rpm < rpm {
-            print!("  SHIFT");
+            print!("{:>8}", " SHIFT ");
         }
+
+        print!("{:#?}", flags);
 
         println!("");
     }
